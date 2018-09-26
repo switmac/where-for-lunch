c018762e65f09058b4895925309f960a7f797e7b
Marie Antoinette Go <marieantoinette.go@gmail.com>
2018-09-27 07:11:27 +0800

    task-1 disable search button when lat_long is undefined

--- client/components/Button/Button.js
+++ client/components/Button/Button.js
@@ -3,14 +3,16 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import styles from './Button.css';
 
-const Button = ({ onClick, children, theme }) => {
+const Button = ({ onClick, children, theme, isDisabled }) => {
+  const clickHandler = isDisabled ? null : onClick;
   return (
     <button
       className={classNames({
         [styles.root]: true,
         [styles[theme]]: true,
+        disabled: isDisabled,
       })}
-      onClick={onClick}
+      onClick={clickHandler}
     >
       { children }
     </button>
@@ -23,6 +25,7 @@ Button.propTypes = {
   ]),
   onClick: PropTypes.func,
   theme: PropTypes.string,
+  isDisabled: PropTypes.bool,
 };
 
 export default Button;
