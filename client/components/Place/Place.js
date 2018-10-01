f7da73efc60e98910db0963511f68c29a34c505f
Marie Antoinette Go <marieantoinette.go@gmail.com>
2018-10-02 01:12:26 +0800

    task-3 add place component details

--- client/components/Place/Place.js
+++ client/components/Place/Place.js
@@ -1,32 +1,63 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import styles from './Place.css';
 
-const Place = ({ place }) => {
-  return (
-    <div className={styles.root}>
-      <div className={styles.name}>{ place.name || 'Where for lunch?' }</div>
-      <div className={styles.box}>
-        <div>{ place.address }</div>
-        <div>{ place.phone }</div>
-        <div>{ place.categories && place.categories.join(', ') }</div>
-        <div>{ place.price }</div>
-        { place.rating &&
-        <div className={styles.rating}>
-          <div className={styles.ratingScore}>{ place.reviewCount } reviews</div>
-          <div className={styles.stars}>
-            <div className={styles.emptyStars}></div>
-            <div className={styles.fullStars} style={{ width: `${place.rating / 5 * 100}%` }}></div>
-          </div>
+export default class Place extends PureComponent {
+  static propTypes = {
+    place: PropTypes.object,
+    onClick: PropTypes.func,
+  };
+
+  handleOnClick = () => {
+    const { place, onClick } = this.props;
+    onClick && onClick(place);
+  };
+
+  render() {
+    const { place, onClick } = this.props;
+    const { name, hours, photos, address, phone, categories, price, rating } = place;
+    const header = place.name ? <Link to="/place" className={styles.link} onClick={ this.handleOnClick }>{ name }</Link> : 'Where for lunch?';
+    const photoList = photos ? photos.map((photo, index) => {
+      return <div key={ index } className={styles.photo}><img src={ photo } /></div>;
+    }) : '';
+    const businessHours = hours ? hours.map((hour, index) => {
+      return <div key={ index } className={ styles.storeHours }>
+        <div className={ styles.hoursType }> { hour.hoursType }</div>
+        <div className={ styles.isOpenNow }> { hour.isOpenNow ? 'OPEN NOW' : 'CLOSE NOW'}</div>
+      </div>;
+    }) : '';
+
+
+    return (
+      <div className={styles.root} >
+        <div className={styles.name}>{ header } </div>
+        <div className={styles.box}>
+          <div>{ address }</div>
+          <div>{ phone }</div>
+          <div>{ categories && categories.join(', ') }</div>
+          <div>{ price }</div>
+          { rating &&
+            <div className={styles.rating}>
+              <div className={styles.ratingScore}>{ place.reviewCount } reviews</div>
+              <div className={styles.stars}>
+                <div className={styles.emptyStars}></div>
+                <div className={styles.fullStars} style={{ width: `${place.rating / 5 * 100}%` }}></div>
+              </div>
+            </div>
+          }
+          { businessHours && !onClick &&
+            <div className={styles.businessHours}>
+              { businessHours }
+            </div>
+          }
+          { photoList && !onClick &&
+            <div className={styles.photos}>
+              { photoList }
+            </div>
+          }
         </div>
-        }
       </div>
-    </div>
-  );
-};
-
-Place.propTypes = {
-  place: PropTypes.object,
-};
-
-export default Place;
+    );
+  }
+}
